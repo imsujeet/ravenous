fedad69dbedc3d55736af526c7a81ca7d0f780be
Sujeet <iamsujeet1308@gmail.com>
2020-09-09 20:23:48 +0530

    communicated with yelp api using Yelp.js file and fetched results and displayed

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,14 +4,27 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
     render() {
-        return (
+        if(this.props.businesses.length > 0){
+            return (
+            
+                <div className="BusinessList">
+                    
+                    {this.props.businesses.map(business => {
+                        return <Business business = {business} key = {business.id}/>;
+                    })}
+                    
+                </div>
+            )
+
+        }else {
+            return (
             <div className="BusinessList">
-                {this.props.businesses.map(business => {
-                    return <Business business = {business} />;
-                })}
-                
-            </div>
-        )
+                <h2>Search something!!</h2>
+                <br />
+                <p>Try setting the serach location to any US city.</p>
+            </div>);
+        }
+        
     }
 }
 
